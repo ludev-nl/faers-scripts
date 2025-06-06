943da7c4540ef9c1e11cbcd22921767b370190e2
Noah Klein <noahkleinnl@duck.com>
2025-06-06 16:59:12 +0200

    Added an special check-where-logs-folder-is-or-create-it function, so now we start the logger without cyclic dependencies.

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,3 @@
-# TODO: add pdf option functionality. SKIP
-# test for all 4 schema variations
-# errors often propagate up to this file, and the source is hard to find. DONE
-# can we add backtraces of some kind? DONE
-# add unit testing
-# add error handling everywhere
-# add function descriptions everywhere
-# add file headers everywhere DONE
-# clean up all files
-
 import argparse
 import os
 
@@ -40,6 +30,8 @@ def main():
     )
     args = parser.parse_args()
 
+    # This should be the first call to the logging framework,
+    # so this call with also properly set up the logger.
     log = get_logger()
 
     data_dir = get_option_from_json(OPTIONS_DIR, "root_data_dir")
--- src/constants.py
+++ src/constants.py
@@ -4,15 +4,17 @@ Pipeline wide: Hard-coded constants.
 
 import os
 
-from option import get_option_from_json
+# from option import get_option_from_json
 
 # These directories are hard-coded, the rest is specified
 # in the options file.
 CONFIG_DIR = os.path.abspath('config')
 OPTIONS_DIR = os.path.abspath('config/general_options.json')
+# This is unfortunately hard coded.
+LOG_DIR = os.path.abspath('faers_data/logs')
 
 # obtain values from option file
-ROOT_DATA_DIR = get_option_from_json(
-    OPTIONS_DIR,
-    "root_data_dir"
-)
+# ROOT_DATA_DIR = get_option_from_json(
+#     OPTIONS_DIR,
+#     "root_data_dir"
+# )
